6a7727775cd2f85cdbc7e7b899917a445f44e738
sourav saha <souravsaha675@gmail.com>
2020-10-16 23:54:27 +0530

    Posting data in firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummery from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -74,7 +75,25 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You Continue!");
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: "Sourav saha",
+        address: {
+          street: "TestStreet",
+          zipcode: "123456",
+          country: "UK",
+        },
+        email: "Souravsaha@gmail.com",
+      },
+      deliveryMethod: "Fastest",
+    };
+
+    axios
+      .post("/orders.json", order)
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error));
   };
 
   render() {
