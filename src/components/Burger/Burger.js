e5367da704b028d52163be1bb7937760f00568d8
sourav saha <souravsaha675@gmail.com>
2020-10-19 03:33:16 +0530

    Implementing Routing in this project

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import classes from "./Burger.module.css";
 import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
 const burger = (props) => {
+  //console.log(props);
   let transformedIngredients = Object.keys(props.ingredients)
     .map((ingredientKey) => {
       return [...Array(props.ingredients[ingredientKey])].map((_, i) => {
@@ -31,4 +33,4 @@ const burger = (props) => {
   );
 };
 
-export default burger;
+export default withRouter(burger);
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -83,26 +83,42 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    this.setState({ loading: true });
-    const order = {
-      ingredients: this.state.ingredients,
-      price: this.state.totalPrice,
-      customer: {
-        name: "Sourav saha",
-        address: {
-          street: "TestStreet",
-          zipcode: "123456",
-          country: "UK",
-        },
-        email: "Souravsaha@gmail.com",
-      },
-      deliveryMethod: "Fastest",
-    };
+    //   this.setState({ loading: true });
+    //   const order = {
+    //     ingredients: this.state.ingredients,
+    //     price: this.state.totalPrice,
+    //     customer: {
+    //       name: "Sourav saha",
+    //       address: {
+    //         street: "TestStreet",
+    //         zipcode: "123456",
+    //         country: "UK",
+    //       },
+    //       email: "Souravsaha@gmail.com",
+    //     },
+    //     deliveryMethod: "Fastest",
+    //   };
 
-    axios
-      .post("/orders.json", order)
-      .then((response) => this.setState({ loading: false, purchasing: false }))
-      .catch((error) => this.setState({ loading: false, purchasing: false }));
+    //   axios
+    //     .post("/orders.json", order)
+    //     .then((response) => this.setState({ loading: false, purchasing: false }))
+    //     .catch((error) => this.setState({ loading: false, purchasing: false }));
+
+    const queryParams = [];
+    for (let i in this.state.ingredients) {
+      queryParams.push(
+        encodeURIComponent(i) +
+          "=" +
+          encodeURIComponent(this.state.ingredients[i])
+      );
+    }
+
+    const queryString = queryParams.join("&");
+
+    this.props.history.push({
+      pathname: "/checkout",
+      search: "?" + queryString,
+    });
   };
 
   render() {
