a74b9a16c04ac85a6f00dd70339e66d37917f17b
sourav saha <souravsaha675@gmail.com>
2020-10-21 03:24:50 +0530

    Adding Order page &  Fetching order from database

---
+++ src/components/Order/Order.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import classes from "./Order.module.css";
+
+const order = (props) => {
+  const ingredients = [];
+  for (let ingredient in props.ingredients) {
+    ingredients.push({
+      name: ingredient,
+      amount: props.ingredients[ingredient],
+    });
+  }
+  //console.log(ingredients);
+  //console.log(props.ingredients);
+  const ingredientOutput = ingredients.map((el) => {
+    return (
+      <span
+        style={{
+          textTransform: "capitalize",
+          display: "inline-block",
+          margin: "0 8px",
+          border: "1px solid #ccc",
+          padding: "5px",
+        }}
+        key={el.name}
+      >
+        {el.name} ({el.amount})
+      </span>
+    );
+  });
+  return (
+    <div className={classes.Order}>
+      <p>Ingredients : {ingredientOutput}</p>
+      <p>
+        Price :<strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong>
+      </p>
+    </div>
+  );
+};
+
+export default order;
