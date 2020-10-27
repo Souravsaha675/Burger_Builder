e959c3d007b2b0e35367f4ea7333f6d13d223815
sourav saha <souravsaha675@gmail.com>
2020-10-27 23:46:08 +0530

    Handiling From validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -16,6 +16,11 @@ class ContactData extends Component {
           placeholder: "Your name",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       street: {
         elementType: "input",
@@ -24,6 +29,11 @@ class ContactData extends Component {
           placeholder: "Street",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       zipcode: {
         elementType: "input",
@@ -32,6 +42,13 @@ class ContactData extends Component {
           placeholder: "Zip Code",
         },
         value: "",
+        validation: {
+          required: true,
+          minLength: 5,
+          maxLength: 5,
+        },
+        valid: false,
+        touched: false,
       },
       country: {
         elementType: "input",
@@ -40,6 +57,11 @@ class ContactData extends Component {
           placeholder: "Country",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       email: {
         elementType: "input",
@@ -48,6 +70,11 @@ class ContactData extends Component {
           placeholder: "Your E-Mail",
         },
         value: "",
+        validation: {
+          required: true,
+        },
+        valid: false,
+        touched: false,
       },
       deliveryMethod: {
         elementType: "select",
@@ -62,6 +89,23 @@ class ContactData extends Component {
     },
     loading: false,
   };
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if (rules.required) {
+      isValid = value.trim() !== "" && isValid;
+    }
+
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+
+    if (rules.maxLength) {
+      isValid = value.length >= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
 
   orderHandler = (event) => {
     event.preventDefault();
@@ -90,6 +134,11 @@ class ContactData extends Component {
     const updatedFromElement = { ...updatedOrderFrom[inputIdentifier] };
 
     updatedFromElement.value = event.target.value;
+    updatedFromElement.valid = this.checkValidity(
+      updatedFromElement.value,
+      updatedFromElement.validation
+    );
+    updatedFromElement.touched = true;
     updatedOrderFrom[inputIdentifier] = updatedFromElement;
     this.setState({ orderForm: updatedOrderFrom });
   };
@@ -111,6 +160,9 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)}
           />
         ))}
